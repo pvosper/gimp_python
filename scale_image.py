4d7ba747f5892436f9fc515d3dd22c31fd86cf9b
pvosper <pvosper@mac.com>
2017-11-13 12:31:53 -0800

    Working correctly at last!  Note image.width

--- scale_image.py
+++ scale_image.py
@@ -3,15 +3,22 @@
 from gimpfu import *
 
 
-# method must be registered
-def scale_image(image, number):
-    pdb.gimp_image_scale(image, image.width*number, image.height*number)
+def scale_image(image, max):
+
+    if image.width > image.height:
+        scale = float(max) / image.width
+    else:
+        scale = float(max) / image.height
+
+    pdb.gimp_image_scale(image, image.width*scale, image.height*scale)
+
     return
+
 
 register(
     "scale_image",                          # proc_name called from command line
-    "Scales image within specified limit",  # info about plug-in
-    "help message",                         # help
+    "Scale image within max bounds",            # info about plug-in
+    "Scales image within specified maximum bounds while maintaining original aspect ratio",                         # help
     "Paul Vosper",                          # author
     "Paul Vosper",                          # copyright holder for the plug-in
     "2017",                                 # copyright date
@@ -19,7 +26,7 @@ register(
     "*",                                    # image types ie: RGB*, GRAY* &etc
     [
         (PF_IMAGE, "image", "Input image", None),
-        (PF_INT, "max", "Max?", 640)
+        (PF_INT, "max", "Max?", 320)
     ],                                      # method parameters
     [],                                     # method results
     scale_image,                            # name of method that will be called
