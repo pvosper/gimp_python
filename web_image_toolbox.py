8e3ea2552e082a491ca39f7ad7db42d5d1f53da0
pvosper <pvosper@mac.com>
2017-11-21 07:31:42 -0800

    Cleanup and add support for working files

--- web_image_toolbox.py
+++ web_image_toolbox.py
@@ -12,16 +12,26 @@ from gimpfu import *
 
 def web_toolbox(image, drawable, maximum_dimension, path, filename):
 
-    if image.width > image.height:
-        scale = float(maximum_dimension) / image.width
+    # Create working image for destructive manipulation
+    working_image = pdb.gimp_image_duplicate(image)
+    working_layer = pdb.gimp_image_merge_visible_layers(working_image, CLIP_TO_IMAGE)
+
+    # Create scale factor to fit working image
+    # todo rectangular limits as option, rather than square
+    if working_image.width > working_image.height:
+        scale = float(maximum_dimension) / working_image.width
     else:
-        scale = float(maximum_dimension) / image.height
+        scale = float(maximum_dimension) / working_image.height
 
+    # Scale and adjust working image
     pdb.gimp_context_set_interpolation(3)
-    pdb.gimp_image_scale(image, image.width*scale, image.height*scale)
-    pdb.plug_in_unsharp_mask(image, drawable, 5.0, 0.5, 0)
+    pdb.gimp_image_scale(working_image, working_image.width*scale, working_image.height*scale)
+    pdb.plug_in_unsharp_mask(working_image, working_layer, 5.0, 0.5, 0)
 
-    pdb.file_png_save(image, drawable, '/Users/paulvosper/temp/temp.png', 'temp.png', 0, 9, 1, 1, 1, 1, 1)
+    # Export working image as PNG
+    pdb.file_png_save(working_image, working_layer, '/Users/paulvosper/temp/temp.png', 'temp.png', 0, 9, 1, 1, 1, 1, 1)
+
+    pdb.gimp_image_delete(working_image)
 
     return
 
