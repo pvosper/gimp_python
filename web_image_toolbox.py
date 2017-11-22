5ec8cd252c050696f92af11b1801863b19109b46
pvosper <pvosper@mac.com>
2017-11-22 08:35:28 -0800

    v0.001: w00t! Now (barely) functional as intended (still lots of work to do)

--- web_image_toolbox.py
+++ web_image_toolbox.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 
 """Image scaling for display on personal web site
-    1280 - Enlarge
-    640 - Page
-    320 - Index
-    180 - Thumbnail
+    2048 - Zoom
+    1024 - Display
+    512 - Index
     Each should be derived from original (not chained)"""
 
 from gimpfu import *
@@ -17,36 +16,38 @@ home_dir = os.path.expanduser('~')
 def web_toolbox(image, drawable, name):
     # 'Specify as many input parameters as there are in the plugin_func and in the same order'
 
-    # todo: Loop through sizes, naming each uniquely
+    export_sizes = [('zoom', 2048), ('display', 1024), ('index', 512)]
 
-    # Create working image for destructive manipulation
-    working_image = pdb.gimp_image_duplicate(image)
-    working_layer = pdb.gimp_image_merge_visible_layers(working_image, CLIP_TO_IMAGE)
+    for export in export_sizes:
 
-    maximum_dimension = 320
+        # Create working image for destructive manipulation
+        working_image = pdb.gimp_image_duplicate(image)
+        working_layer = pdb.gimp_image_merge_visible_layers(working_image, CLIP_TO_IMAGE)
 
-    # Create scale factor to fit working image
-    # todo: Use rectangular limits (ie, width != height) as option, rather than square
-    if working_image.width > working_image.height:
-        scale = float(maximum_dimension) / working_image.width
-    else:
-        scale = float(maximum_dimension) / working_image.height
+        maximum_dimension = export[1]
 
-    # Scale and adjust working image
-    pdb.gimp_context_set_interpolation(3)
-    pdb.gimp_image_scale(working_image, working_image.width*scale, working_image.height*scale)
-    pdb.plug_in_unsharp_mask(working_image, working_layer, 5.0, 0.5, 0)
+        # Create scale factor to fit working image
+        # todo: Use rectangular limits (ie, width != height) as option, rather than square
+        if working_image.width > working_image.height:
+            scale = float(maximum_dimension) / working_image.width
+        else:
+            scale = float(maximum_dimension) / working_image.height
 
-    # File & path
-    export_file_path = home_dir + '/temp/'
-    export_file_name = name + '.png'
-    export_file = export_file_path + export_file_name
+        # Scale and adjust working image
+        pdb.gimp_context_set_interpolation(3)
+        pdb.gimp_image_scale(working_image, working_image.width*scale, working_image.height*scale)
+        pdb.plug_in_unsharp_mask(working_image, working_layer, 5.0, 0.5, 0)
 
-    # Export working image as PNG
-    pdb.file_png_save(working_image, working_layer, export_file, export_file_name, 0, 9, 1, 1, 1, 1, 1)
+        # File & path
+        export_file_path = home_dir + '/temp/'
+        export_file_name = name + '_' + export[0] + '.png'
+        export_file = export_file_path + export_file_name
 
-    # Clean up working image
-    pdb.gimp_image_delete(working_image)
+        # Export working image as PNG
+        pdb.file_png_save(working_image, working_layer, export_file, export_file_name, 0, 9, 1, 1, 1, 1, 1)
+
+        # Clean up working image
+        pdb.gimp_image_delete(working_image)
 
     return
 
