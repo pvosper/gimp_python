bd0d888fbb3483014bd0be5f152cc5c6cb0ce47d
pvosper <pvosper@mac.com>
2017-11-26 07:50:06 -0800

    Implemented width and height scale factors

--- web_image_toolbox.py
+++ web_image_toolbox.py
@@ -18,7 +18,7 @@ home_dir = os.path.expanduser('~')
 
 def web_toolbox(image, drawable, project, title, dir, zoo_w, zoo_h, dis_w, dis_h, ind_w, ind_h):
 
-    export_sizes = [('zoo', 2048), ('dis', 1024), ('ind', 512)]
+    export_sizes = [('zoo', zoo_w, zoo_h), ('dis', dis_w, dis_h), ('ind', ind_w, ind_h)]
 
     for export in export_sizes:
 
@@ -27,19 +27,33 @@ def web_toolbox(image, drawable, project, title, dir, zoo_w, zoo_h, dis_w, dis_h
         working_layer = pdb.gimp_image_merge_visible_layers(working_image, CLIP_TO_IMAGE)
 
         size_label = '_' + export[0]
-        maximum_dimension = export[1]
+        size_width = float(export[1])
+        size_height = float(export[2])
 
         # Create scale factor to fit working image
         # todo: Use rectangular limits (ie, width != height) as option, rather than square
-        if working_image.width > working_image.height:
-            scale = float(maximum_dimension) / working_image.width
-        else:
-            scale = float(maximum_dimension) / working_image.height
+        # Don't scale twice!
+        # if working_image.width > working_image.height:
+        #     scale = float(maximum_dimension) / working_image.width
+        # else:
+        #     scale = float(maximum_dimension) / working_image.height
+        # Find scale factor for width and height - lowest will be most significant scaling
+        width_scale =  size_width / working_image.width
+        height_scale =  size_height / working_image.height
 
         # Scale and adjust working image
-        pdb.gimp_context_set_interpolation(3)
-        pdb.gimp_image_scale(working_image, working_image.width*scale, working_image.height*scale)
-        pdb.plug_in_unsharp_mask(working_image, working_layer, 5.0, 0.5, 0)
+        if width_scale >=1 and height_scale >= 1:
+            pass
+        elif width_scale <= height_scale:
+            pdb.gimp_context_set_interpolation(3)
+            pdb.gimp_image_scale(working_image, working_image.width*width_scale, working_image.height*width_scale)
+            # Always scale uniformly
+            pdb.plug_in_unsharp_mask(working_image, working_layer, 5.0, 0.5, 0)
+        elif width_scale > height_scale:
+            pdb.gimp_context_set_interpolation(3)
+            pdb.gimp_image_scale(working_image, working_image.width*height_scale, working_image.height*height_scale)
+            # Always scale uniformly
+            pdb.plug_in_unsharp_mask(working_image, working_layer, 5.0, 0.5, 0)
 
         # Define file & path
         export_file_path = home_dir + '/temp/'
@@ -73,10 +87,10 @@ register(
         (PF_DIRNAME, "dir", "Directory", home_dir),
         (PF_INT, "zoo_w", "Zoom Width", 1024),
         (PF_INT, "zoo_h", "Zoom Height", 768),
-        (PF_INT, "dis_w", "Display Width", 1024),
-        (PF_INT, "dis_h", "Display Height", 768),
-        (PF_INT, "ind_w", "Index Width", 1024),
-        (PF_INT, "ind_h", "Index Height", 768),
+        (PF_INT, "dis_w", "Display Width", 512),
+        (PF_INT, "dis_h", "Display Height", 384),
+        (PF_INT, "ind_w", "Index Width", 256),
+        (PF_INT, "ind_h", "Index Height", 192),
     ],                                      # method parameters
     [],                                     # method results
     web_toolbox,                            # name of method that will be called
